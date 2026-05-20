764a3a2bbd7c0f3595941ce1fba16d6b9fdcefc1
glorysay96-aw <glory.say.96@gmail.com>
2026-05-20 18:51:01 +0200

    add hero image and modern homepage design

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
@@ -6,6 +7,15 @@ from wagtail.admin.panels import FieldPanel
 class HomePage(Page):
     intro = RichTextField(blank=True)
 
+    hero_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
     content_panels = Page.content_panels + [
         FieldPanel("intro"),
+        FieldPanel("hero_image"),
     ]
